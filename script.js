70f51f49667ced8b9c0ece69756018ddfa78de9a
turarka <77821414+turarka@users.noreply.github.com>
2022-04-08 13:19:38 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,193 @@
+const responsiveNavbar = (function () {
+	const button = document.querySelector("#menuButton");
+	const navbar = document.querySelector("#navbar")
+	button.addEventListener("click", function () {
+		if (navbar.className === "navbar") {
+			navbar.className += " navbarResponsive";
+		}
+		else {
+			navbar.className = "navbar";
+		}
+	});
+})();
+
+var input,search,pr,result,result_arr, locale_HTML, result_store;
+
+function func() {
+ 	locale_HTML = document.body.innerHTML;   // сохраняем в переменную весь body (Первоначальный)
+}
+setTimeout(func, 1000);  //ждем подгрузки Jsona и выполняем
+
+function FindOnPage(name, status) {
+
+	input = document.getElementById(name).value; //получаем значение из поля в html
+	
+	if(input.length<3&&status==true)
+	{
+		alert('Для поиска вы должны ввести три или более символов');
+		function FindOnPageBack() { document.body.innerHTML = locale_HTML; }
+	}
+	
+	if(input.length>=3)
+	{
+		function FindOnPageGo() {
+
+			search = '/'+input+'/g';  //делаем из строки регуярное выражение
+			pr = document.body.innerHTML;   // сохраняем в переменную весь body
+			result = pr.match(/>(.*?)</g);  //отсекаем все теги и получаем только текст
+			result_arr = [];   //в этом массиве будем хранить результат работы (подсветку)
+
+			var warning = true;
+			for(var i=0;i<result.length;i++) {
+				if(result[i].match(eval(search))!=null) {
+					warning = false;
+				}
+			}
+			if(warning == true) {
+				alert('Не найдено ни одного совпадения');
+			}
+
+			for(var i=0; i<result.length;i++) {
+				result_arr[i] = result[i].replace(eval(search), '<span style="background-color:red;">'+input+'</span>'); //находим нужные элементы, задаем стиль и сохраняем в новый массив
+			    //document.getElementById(text-to-find).scrollIntoView();
+			}
+			for(var i=0; i<result.length;i++) {
+				pr=pr.replace(result[i],result_arr[i])  //заменяем в переменной с html текст на новый из новогом ассива
+			}
+			document.body.innerHTML = pr;  //заменяем html код
+		}
+	}
+	function FindOnPageBack() { document.body.innerHTML = locale_HTML; }
+	if(status) { FindOnPageBack(); FindOnPageGo(); } //чистим прошлое и Выделяем найденное
+	if(!status) { FindOnPageBack(); } //Снимаем выделение
+}
+
+
+if (document.getElementById('hearderSlide')) {
+	$('#hearderSlide').multislider();
+	$('#hearderSlide').multislider('pause');
+}
+
+
+function closeCart() {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling')
+}
+
+
+const openShopCart = document.querySelector('.shoppingCartButton');
+openShopCart.addEventListener('click', () => {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling');
+});
+
+
+const closeShopCart = document.querySelector('#closeButton');
+const overlay = document.querySelector('.overlay');
+closeShopCart.addEventListener('click', closeCart);
+overlay.addEventListener('click', closeCart);
+
+
+ function filter() {
+    let input = document.getElementById('text-to-find');
+
+    input.addEventListener('keyup', function filter() {
+        let filter = input.value.toLowerCase(),
+            filterElements = document.getElementBy('productName');
+
+        filterElements.forEach(item => {
+            if (item.innerHTML.toLowerCase().indexOf(filter) > -1) {
+                item.style.display = '';
+            } else {
+                item.style.display = 'none';
+            }
+        })
+
+    })
+
+}
+
+
+
+const btns = document.querySelectorAll('.btn');
+const storeProducts = document.querySelectorAll('.product-list');
+// const search = document.getElementById(search);
+
+for (i = 0; i < btns.length; i++) {
+
+    btns[i].addEventListener('click', (e) => {
+        e.preventDefault()
+        
+        const filter = e.target.dataset.filter;
+        console.log(filter);
+        
+        storeProducts.forEach((product)=> {
+            if (filter === 'all'){
+                product.style.display = 'block'
+            } else {
+                if (product.classList.contains(filter)){
+                    product.style.display = 'block'
+                } else {
+                    product.style.display = 'none'
+                }
+            }
+        });
+    });
+};
+
+
+
+const search = () => {
+	const searchbox = document.getElementById("search-item").value.toUpperCase();
+	const storeitems = document.getElementById("product-list")
+	const product = document.querySelectorAll(".product")
+	const pname = storeitems.getElementById("name")
+
+	for (var i = 0; i < pname.length; i++){
+		let match = product[i].getElementById("name")[0];
+
+		if(match) {
+			let textvalue = match.textContent || match.innerHTML
+
+			if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+				product[i].style.display = "";			  }
+		} else {
+			product[i].style.display = "none";
+		}
+	}
+}
+
+
+function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+    //select all cards
+    let elements = document.querySelectorAll(".product");
+    //loop through all cards
+    elements.forEach((element) => {
+      //display all cards on 'all' button click
+      if (value == "all") {
+        element.classList.remove("hide");
+      } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+          element.classList.remove("hide");
+        } else {
+          //hide other elements
+          element.classList.add("hide");
+        }
+      }
+    });
+  }
+
